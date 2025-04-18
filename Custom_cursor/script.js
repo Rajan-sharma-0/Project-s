814abc1584862de4f4f07df6d3aad80d985e4a44
Rajan Sharma <140615149+Rajan-sharma-0@users.noreply.github.com>
2025-04-18 15:12:00 +0530

    Add files via upload

---
+++ Custom_cursor/script.js
@@ -0,0 +1,7 @@
+let body = document.querySelector("#main");
+let curser = document.querySelector("#cursor");
+
+body.addEventListener("mousemove", function(e){
+  curser.style.left = e.x + "px";
+  curser.style.top = e.y + "px";
+});
